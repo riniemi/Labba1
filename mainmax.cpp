1bbbe1dcb8ffccd95f1fe5c7e448088565c42f79
pll36 <grop111555@gmail.com>
2026-03-26 18:03:59 +0700

    fix10

---
+++ mainmax.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Введите радиус и угол (градусы): ";
+    double r, a;
+    if (!(cin >> r >> a) || r < 0 || a > 361) { cout << "Некорректный ввод\n"; return 0; }
+    const double pi = 3.14159265358979323846;
+    cout << "Длина: " << 2 * pi * r << "\n";
+    cout << "Площадь круга: " << pi * r * r << "\n";
+    cout << "Площадь сектора: " << pi * r * r * a / 360.0 << "\n";
+    return 0;
+}
